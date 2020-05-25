51204bea2658ff26b48c18ec2fcb1a8ccf26d3b8
Zurum <50442301+engrceey@users.noreply.github.com>
2020-05-25 23:17:39 +0100

    uploading a quiz app

---
+++ main.js
@@ -0,0 +1,57 @@
+const quiz = [
+  {
+    q: "What Breaks and never falls and what falls and never breaks ?",
+    options: ["Air", "Day breaks and night falls", "Sun shine", "Sound"],
+    answer: 1,
+  },
+  {
+    q: "Some months have 31 days, others have 30 days, but how many have 28 ?",
+    options: ["Febuary", "One month", "All months", "Some months"],
+    answer: 2,
+  },
+  {
+    q:
+      "If a plane crashes on the border between the USA and Canada, Where will they bury the survivors ?",
+    options: [
+      "The area the crash happened",
+      "Will be buried in their countries",
+      "In the border",
+      "No survivor will be buried",
+    ],
+    answer: 3,
+  },
+  {
+    q:
+      "If You have a bowl with six apples if you take four,how many do you have?",
+    options: ["Four", "Two", "Six", "None"],
+    answer: 0,
+  },
+  {
+    q: "What can be broken but is never held ?",
+    options: ["Heart", "Air", "Promise", "Lies"],
+    answer: 2,
+  },
+  {
+    q: "What can live if its feed and dies if it drinks ?",
+    options: ["Fish", "Rumours", "Heart", "Fire"],
+    answer: 3,
+  },
+];
+
+let positive = [
+  "CORRECT! BOSS!! &#128372;",
+  "CORRECT! NA SO!! &#128588;",
+  "CORRECT! I BELIEVE YOU!! &#128077;",
+  "CORRECT! SMASHED!! &#128076;",
+  "CORRECT! FINISH WORK!! &#128079;",
+  "CORRECT! YOU SABI SOMETHING &#127948;",
+];
+
+let negative = [
+  "WRONG! SEE YOUR LIFE!! &#128530;",
+  "WRONG! E!! SHOCK YOU ABI!! &#128514;",
+  "WRONG! COCONUT HEAD!! &#128566;",
+  "WRONG! YOU NO SABI ANYTHING!! &#128580;",
+  "WRONG! SIMPLE QUESTION YOU FAIL!! &#128580;",
+  "WRONG! CHAI! SO YOU FAIL THIS THING!! &#128563;",
+];
